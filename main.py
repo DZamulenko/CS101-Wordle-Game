17a0cd6494fa56601920ebc946456432357c61b3
dmitro <@user>
2025-11-20 11:16:14 +0200

    implemented game loop and input validation

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 import random
 words=["glove","serve","stuck","block","haven","craft","hello","build","stone","apple"]
 secrets_word=random.choice(words)
+attempts=6
+while attempts!=0:
+    user_word=input()
+    if len(user_word)!=5:
+        print("error. word length is less than 5 ")
+    else:
+        attempts-=1
