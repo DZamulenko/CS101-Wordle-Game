31d9e60cbabaecdab3eda3c183c38587ff9f29a5
dmitro <@user>
2025-11-20 12:59:52 +0200

    added logic for comparing words

--- main.py
+++ main.py
@@ -3,8 +3,18 @@ words=["glove","serve","stuck","block","haven","craft","hello","build","stone","
 secrets_word=random.choice(words)
 attempts=6
 while attempts!=0:
+    correct_letters=[-1,-1,-1,-1,-1]
     user_word=input()
     if len(user_word)!=5:
         print("error. word length is less than 5 ")
     else:
         attempts-=1
+        for i in range(5):
+            if user_word[i]==secrets_word[i]:
+                correct_letters[i]=1
+            else:
+                for j in range(i,5):
+                    if user_word[j]==secrets_word[j]:
+                        correct_letters[j]=0
+                        break
+                break
