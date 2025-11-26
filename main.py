c4880b71ef43ae22651de375a61703d8a23401ff
dmitro <@user>
2025-11-26 16:26:04 +0200

    last bugfix

--- main.py
+++ main.py
@@ -2,43 +2,32 @@ import random
 print("*** ВІТАЄМО У WORDLE ***")
 print("У вас є 6 спроб, щоб вгадати слово з 5 літер.\n")
 words=["glove","serve","stuck","block","haven","craft","hello","build","stone","apple"]
-secrets_word=random.choice(words)
+secrets_word=list(random.choice(words))
 attempts=6
-sum_cor_let=0
+correct=True
 while attempts>0:
     print(f"Спроба {7-attempts}/6")
-    #на почату значення всіх елементів масиву -1 і ми вважаємо що слова взагалі не схожі
-    correct_letters=[-1,-1,-1,-1,-1]
-    user_word=input("Введіть слово: ")
-    #валідація
+    user_word=list(input("Введіть слово: "))
+    correct=True
     if len(user_word)!=5:
         print("error. word length is don't equal 5 ")
     else:
         attempts-=1
-        #перевіряємо літери на відповідність
         for i in range(5):
-            if user_word[i]==secrets_word[i]:
-                correct_letters[i]=1
+            if secrets_word[i]==user_word[i]:
+                user_word[i]='['+user_word[i]+']'
             else:
-                #у випадку не відповідності перевіряємо літеру на наявність у масиві
-                for j in range(5):
-                    if user_word[i]==secrets_word[j]:
-                        correct_letters[i]=0
+                correct=False
+                for j in range(i,5):
+                    if secrets_word[i]==user_word[j]:
+                        user_word[j]='('+user_word[j]+')'
                         break
-        print("результат:",end=" ")
-        for i in range(5):
-            if correct_letters[i]==1:
-                print(f"[{user_word[i]}]",end=" ")
-            elif correct_letters[i]==0:
-                print(f"({user_word[i]})",end=" ")
-            else:
-                print(f"{user_word[i]}", end=" ")
-        print("\n")
-        sum_cor_let=sum(correct_letters)
-        if sum_cor_let==5:
-            print(f"ВІТАЄМО! Ви вгадали слово {secrets_word} на {6-attempts} раз!")
+        print("результат:",user_word)
+        if correct:
+            print("Вітаю ви вгадали")
+            print("Загаданим словом було",end=" ")
+            for i in user_word:
+                print(i[1],end="")
             break
-#якщо цикл завершився і сумма правильних літер дорівнює 5 значит повідомлення про перемогу
-#вивелось, інакше виведеться повідомлення про програш
-if sum_cor_let!=5 :
-    print("На жаль ви програли. Пощастить наступного разу")
+if not correct:
+    print("Нажаль ви не вгадали, спробуйте знову")
